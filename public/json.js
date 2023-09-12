11c59a2e7c408458140325309b5f55cc7ef4b516
Manoel-I <manoel.122112@outlook.com>
2023-09-11 22:38:32 -0300

    JSON for testing

--- public/json.js
+++ public/json.js
@@ -1,25 +1,42 @@
-
-
+// cartas do jogo
 const game_cards = {
     cards :
     [{
+        "id" : 0,
         "img" : '',
         "story" : "x",
         "solution" : "x",
         "read" : false
     },
     {
+        "id" : 1,
         "img" : '',
         "story" : "xxxxxxxxxx",
         "solution" : "x",
         "read" : false
     },
     {
+        "id" : 2,
         "img" : '',
         "story" : "xsasasasasa",
         "solution" : "x",
         "read" : true
-    }] 
+    },
+    {
+        "id" : 3,
+        "img" : '',
+        "story" : "xxxxxxxxxx",
+        "solution" : "x",
+        "read" : false
+    },
+    {
+        "id" : 4,
+        "img" : '',
+        "story" : "xxxxxxxxxx",
+        "solution" : "x",
+        "read" : false
+    },
+    ] 
 };
 // endereço img  
 // numero carta 
