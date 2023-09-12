9fd2f266c4a59b6e140574dc4a1110cd2d41d8b3
Manoel-I <manoel.122112@outlook.com>
2023-09-11 21:07:05 -0300

    initial commit

---
+++ public/functions.js
@@ -0,0 +1,22 @@
+
+
+console.log(game_cards);
+
+function shuffleCards(){ //embaralha as cartas e retorna uma não lida
+    let cardNumber = parseInt( getRandomArbitrary( 0 , game_cards.cards.length));
+
+    while(game_cards.cards[cardNumber].read) {
+        cardNumber = parseInt( getRandomArbitrary( 0 , game_cards.cards.length));
+    }
+    localStorage.setItem("choose_one" , toString( game_cards.cards[cardNumber]));
+    return  game_cards.cards[cardNumber];
+    
+}
+
+function getRandomArbitrary(min, max) {
+    return Math.random() * (max - min) + min;
+}
+
+function readCard(){
+
+}
---
+++ public/json.js
@@ -0,0 +1,29 @@
+
+
+const game_cards = {
+    cards :
+    [{
+        "img" : '',
+        "story" : "x",
+        "solution" : "x",
+        "read" : false
+    },
+    {
+        "img" : '',
+        "story" : "xxxxxxxxxx",
+        "solution" : "x",
+        "read" : false
+    },
+    {
+        "img" : '',
+        "story" : "xsasasasasa",
+        "solution" : "x",
+        "read" : true
+    }] 
+};
+// endereço img  
+// numero carta 
+// historia 
+// solução 
+// lida 
+// 
