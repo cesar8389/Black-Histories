7b98c7a50d3b264e1d9350dd2e9241976e8004f8
Manoel B. C. Neto <manoel.122112@outlook.com>
2023-09-18 00:23:47 -0300

    Changes in file locations for greater organization.

--- public/JSON FILES/cards.js
+++ public/JSON FILES/cards.js
@@ -65,5 +65,101 @@ const game_cards = {
             "solution": "x",
             "read": false
         },
+        {
+            "id": 0,
+            "img": '',
+            "title": "Um copo d'água",
+            "story": " Um homem entra em um bar e pede um copo d'água. O barman puxa uma arma de baixo do balcão e aponta para a cabeça do homem, que responde 'obrigado' e sai do bar.",
+            "solution": "O homem havia entrado no bar para se livrar de soluços, procurando um copo d'água. O barman, percebendo a situação, saca a arma e aponta-a para o homem, a fim de curá-lo com um susto",
+            "read": false
+        },
+        {
+            "id": 1,
+            "img": '',
+            "title": "",
+            "story": "xxxxxxxxxx",
+            "solution": "x",
+            "read": false
+        },
+        {
+            "id": 2,
+            "img": '',
+            "title": "",
+            "story": "xsasasasasa",
+            "solution": "x",
+            "read": true
+        },
+        {
+            "id": 3,
+            "img": '',
+            "title": "",
+            "story": "xxxxxxxxxx",
+            "solution": "x",
+            "read": false
+        },
+        {
+            "id": 4,
+            "img": '',
+            "title": "",
+            "story": "xxxxxxxxxx",
+            "solution": "x",
+            "read": false
+        },
+        {
+            "id": 5,
+            "img": '',
+            "title": "",
+            "story": "xxxxxxxxxx",
+            "solution": "x",
+            "read": false
+        },
+        {
+            "id": 6,
+            "img": '',
+            "title": "",
+            "story": "xxxxxxxxxx",
+            "solution": "x",
+            "read": false
+        },
+        {
+            "id": 7,
+            "img": '',
+            "title": "",
+            "story": "xxxxxxxxxx",
+            "solution": "x",
+            "read": false
+        },
+        {
+            "id": 4,
+            "img": '',
+            "title": "",
+            "story": "xxxxxxxxxx",
+            "solution": "x",
+            "read": false
+        },
+        {
+            "id": 5,
+            "img": '',
+            "title": "",
+            "story": "xxxxxxxxxx",
+            "solution": "x",
+            "read": false
+        },
+        {
+            "id": 6,
+            "img": '',
+            "title": "",
+            "story": "xxxxxxxxxx",
+            "solution": "x",
+            "read": false
+        },
+        {
+            "id": 7,
+            "img": '',
+            "title": "",
+            "story": "xxxxxxxxxx",
+            "solution": "x",
+            "read": false
+        },
         ]
 };
