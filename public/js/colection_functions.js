b4d13150d09fd6d6da5ca9fbe60ded665848a133
Esdras Neres Carneiro <esdras.nc@icloud.com>
2023-09-18 22:09:40 -0300

    ajustes css card colection

--- public/js/colection_functions.js
+++ public/js/colection_functions.js
@@ -5,10 +5,13 @@ console.log(game_cards.cards);
 
 for (const card of game_cards.cards) {
     let div_cards = document.createElement('div');
-    let backgroundImg = document.createElement('img')
+    let backgroundImg = document.createElement('img');
+    let title = document.createElement('p');
+    title.classList.add('title')
     div_cards.classList.add('cards');
-    div_cards.innerText = card.title;
+    title.innerText = card.title;
     backgroundImg.src = card.img;
     div_cards.appendChild (backgroundImg);
+    div_cards.appendChild(title);
     colectionDiv.appendChild(div_cards);
 }
