c025f3453b725d8733e204e5ee244049844a47c5
Esdras Neres Carneiro <esdras.nc@icloud.com>
2023-09-19 16:47:12 -0300

    cards designe

--- public/JSON FILES/cards.js
+++ public/JSON FILES/cards.js
@@ -11,152 +11,152 @@ const game_cards = {
         },
         {
             "id": 1,
-            "img": "./public/img/um_copo.png",
-            "title": "careca que da em cima da irmão dos outros",
+            "img": "./public/img/o_ultimo_trem.png",
+            "title": "O último trem",
             "story": "xxxxxxxxxx",
             "solution": "x",
             "read": false
         },
         {
             "id": 2,
-            "img": '../img/um_copo.png',
-            "title": "",
+            "img": './public/img/uma_porta_aberta.png',
+            "title": "Uma porta aberta",
             "story": "xsasasasasa",
             "solution": "x",
             "read": true
         },
         {
             "id": 3,
-            "img": '../img/um_copo.png',
-            "title": "",
+            "img": './public/img/o_acidente.png',
+            "title": "O acidente",
             "story": "xxxxxxxxxx",
             "solution": "x",
             "read": false
         },
         {
             "id": 4,
-            "img": '../img/um_copo.png',
-            "title": "",
+            "img": './public/img/testemunha_sequestro.png',
+            "title": "Testemunha de um sequestro",
             "story": "xxxxxxxxxx",
             "solution": "x",
             "read": false
         },
         {
             "id": 5,
-            "img": '../img/um_copo.png',
-            "title": "",
+            "img": './public/img/um_roubo.png',
+            "title": "Um roubo sem solução",
             "story": "xxxxxxxxxx",
             "solution": "x",
             "read": false
         },
         {
             "id": 6,
-            "img": '../img/um_copo.png',
-            "title": "",
+            "img": './public/img/doutor.png',
+            "title": "Doutor, doutor",
             "story": "xxxxxxxxxx",
             "solution": "x",
             "read": false
         },
         {
             "id": 7,
-            "img": '../img/um_copo.png',
-            "title": "",
+            "img": './public/img/perdido.png',
+            "title": "Perdido",
             "story": "xxxxxxxxxx",
             "solution": "x",
             "read": false
         },
         {
             "id": 0,
-            "img": '../img/um_copo.png',
-            "title": "Um copo d'água",
-            "story": " Um homem entra em um bar e pede um copo d'água. O barman puxa uma arma de baixo do balcão e aponta para a cabeça do homem, que responde 'obrigado' e sai do bar.",
-            "solution": "O homem havia entrado no bar para se livrar de soluços, procurando um copo d'água. O barman, percebendo a situação, saca a arma e aponta-a para o homem, a fim de curá-lo com um susto",
+            "img": './public/img/splish_splash.png',
+            "title": "Splish, splash!",
+            "story": " ",
+            "solution": "",
             "read": false
         },
         {
             "id": 1,
-            "img": '../img/um_copo.png',
-            "title": "",
+            "img": './public/img/um_assassinato.png',
+            "title": "Um assassinato nada surpreendente",
             "story": "xxxxxxxxxx",
             "solution": "x",
             "read": false
         },
         {
             "id": 2,
-            "img": '../img/um_copo.png',
-            "title": "",
+            "img": './public/img/o_homem_enforcado.png',
+            "title": "O homem enforcado",
             "story": "xsasasasasa",
             "solution": "x",
             "read": true
         },
         {
             "id": 3,
-            "img": '../img/um_copo.png',
-            "title": "",
+            "img": './public/img/tarde_demais.png',
+            "title": "Tarde demais",
             "story": "xxxxxxxxxx",
             "solution": "x",
             "read": false
         },
         {
             "id": 4,
-            "img": '../img/um_copo.png',
-            "title": "",
+            "img": './public/img/a_chave_misteriosa.png',
+            "title": "A chave misteriosa",
             "story": "xxxxxxxxxx",
             "solution": "x",
             "read": false
         },
         {
             "id": 5,
-            "img": '../img/um_copo.png',
-            "title": "",
+            "img": './public/img/ilha_tesouro.png',
+            "title": "Ilha do tesouro",
             "story": "xxxxxxxxxx",
             "solution": "x",
             "read": false
         },
         {
             "id": 6,
-            "img": '../img/um_copo.png',
-            "title": "",
+            "img": './public/img/batida.png',
+            "title": "Batida!",
             "story": "xxxxxxxxxx",
             "solution": "x",
             "read": false
         },
         {
             "id": 7,
-            "img": '../img/um_copo.png',
-            "title": "",
+            "img": './public/img/banho_de_manhã.png',
+            "title": "Banho de manhã",
             "story": "xxxxxxxxxx",
             "solution": "x",
             "read": false
         },
         {
             "id": 4,
-            "img": '../img/um_copo.png',
-            "title": "",
+            "img": './public/img/uma_voz.png',
+            "title": "Uma voz extremamente familiar",
             "story": "xxxxxxxxxx",
             "solution": "x",
             "read": false
         },
         {
             "id": 5,
-            "img": '../img/um_copo.png',
-            "title": "",
+            "img": './public/img/uma_surpresa.png',
+            "title": "Uma surpresa de natal",
             "story": "xxxxxxxxxx",
             "solution": "x",
             "read": false
         },
         {
             "id": 6,
-            "img": '../img/um_copo.png',
-            "title": "",
+            "img": './public/img/pare.png',
+            "title": "Pare",
             "story": "xxxxxxxxxx",
             "solution": "x",
             "read": false
         },
         {
             "id": 7,
-            "img": '../img/um_copo.png',
-            "title": "",
+            "img": './public/img/o_papel.png',
+            "title": "O papel",
             "story": "xxxxxxxxxx",
             "solution": "x",
             "read": false
