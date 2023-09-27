e001d9322c04e0124c04c5af388fda575cf041fc
Manoel B. C. Neto <manoel.122112@outlook.com>
2023-09-27 14:09:11 -0300

    change in modal functions. Change in rules JSON.

--- public/JSON FILES/rules.js
+++ public/JSON FILES/rules.js
@@ -1,6 +1,6 @@
 const rules = {
-    "rule1": "tamandua 01",
-    "rule2": "crocolido 02",
-    "rule3": "andré 03",
-    "rule4": "careca 04"
+    "rule1": "Aqui você pode tirar todas as suas dúvidas sobre o jogo, basta clicar em 'AVANÇAR' para avançar e em 'VOLTAR' para voltar.",
+    "rule2": "Dark Stories deve ser jogado em grupo. Uma pessoa, designada como narradora, escolhe um enigma e lê sua descrição em voz alta",
+    "rule3": "Depois, o narrados lê sua solução sem dizer às outras pessoas. Os outros jogadores deverão fazer perguntas para resolver o enigma. O narrador só pode responder às perguntas usando 'SIM', 'NÃO' ou 'NÃO É RELEVANTE'.",
+    "rule4": "A única solução possível é aquela fornecida na parte de trás de cada carta enigma. Se a resposta ainda não for suficientemente clara, os jogadores devem seguir a interpretação do narrador do enigma. Cabe ao narrador dar algumas pistas se a história estiver em um impasse.",
 };
